8a06b1865b5b47b9647674a845c1587a07f4af24
pat-tan <patricktan1204@gmail.com>
2022-01-25 12:30:25 -0500

    create user context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from 'react'
+import Nav from './components/Nav'
+import UserContext from './contexts/UserContext.js'
+import './App.css'
 
-function App() {
+
+function App () {
+  // To enable use to use our context, we import first, then we can use the useCOntext hook to access our contesxt
+  // const user = useContext(UserContext)
+  // console.log(user)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        <UserContext.Provider value={'Christina'}>
+          <Nav />
+        </UserContext.Provider>
+      }
     </div>
   );
 }
---
+++ src/contexts/UserContext.js
@@ -0,0 +1,7 @@
+//import createContext
+import React, { createContext } from 'react'
+
+//simplest way to create a context
+const UserContext = createContext(null)
+
+export default UserContext
